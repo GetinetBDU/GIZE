11882077d3752ffeb2f83c9439029a9548f9290b
Abdi Belay <tinsasoft@gmail.com>
2017-05-24 00:17:37 -0700

    Tinsae add to direct

---
+++ ChatApp/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    
+    public static void main(String[] args){
+    
+    javax.swing.JOptionPane.showMessageDialog(null,"This is tinsae");
+        
+    }
+    
+    
+}
+
