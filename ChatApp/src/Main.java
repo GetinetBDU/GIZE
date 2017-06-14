2b74d4a9e97dac5aa91cda472fdfc27e41f4aa63
Tsgaye <Tsgaye@10.2.84.75>
2017-06-13 18:21:25 -0700

    This is the first committsage

--- ChatApp/src/Main.java
+++ ChatApp/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
     
     javax.swing.JOptionPane.showMessageDialog(null,"This is tinsae");
-        
+    System.out.println("if you know me you me you don't ask me");    
     }
     
     
